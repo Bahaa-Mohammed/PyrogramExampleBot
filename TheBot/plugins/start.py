13a3eac9dd219be236e65192c31091493ce9dc9d
Bahaa-Mohammed <98849759+Bahaa-Mohammed@users.noreply.github.com>
2022-02-16 16:00:24 +0200

    Update start.py

--- TheBot/plugins/start.py
+++ TheBot/plugins/start.py
@@ -24,12 +24,6 @@ Heya I'm Alive :)""",
                         "Follow On Github", url="https://github.com/Achu2234")]]))
 
 
-@Client.on_message(filters.command(["lovely", "lovely@Pyro_Tg_Bot"]) & filters.private)
-async def start(_, message: Message):
-    await message.reply_text(
-        f"""<b>Hi {message.from_user.first_name} 😉️!</b>
-Heya I'm Alive :)""")
-
 # So now let's make private only command
 
 @Client.on_message(filters.command(["love", "lovee"]) & filters.private)
